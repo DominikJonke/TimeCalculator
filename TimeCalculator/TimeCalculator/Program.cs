eb540bcbb5051afde892f78bd16e2ea88e1e63a0
D J <dominik.jonke@outlook.com>
2020-02-21 16:19:44 +0100

    New code in class

---
+++ TimeCalculator/TimeCalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TimeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time.ShowTitle();
+
+            Time.SetTime();
+
+            Time.CalcTime();
+
+            Time.WriteTime();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TimeCalculator/TimeCalculator/Time.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TimeCalculator
+{
+    public static class Time
+    {
+        public static int time = 0;
+        public static int sec = 0;
+        public static int min = 0;
+        public static int hour = 0;
+
+        public static void ShowTitle()
+        {
+            Console.WriteLine();
+            Console.WriteLine("\t\t\tTime Calculator");
+            Console.WriteLine();
+        }
+        public static void SetTime()
+        {
+            Console.Write("\tTime in seconds: ");
+            time = Convert.ToInt32(Console.ReadLine());
+        }
+        public static void CalcTime()
+        {
+            sec = time % 60;
+            min = time / 60;
+            hour = time / 360;
+        }
+        public static void WriteTime()
+        {
+            Convert.ToString(sec);
+            Convert.ToString(min);
+            Convert.ToString(hour);
+            Console.WriteLine($"\n\t\tTime: {hour:D2}:{min:D2}:{sec:D2}");
+        }
+    }
+}
